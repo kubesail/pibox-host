6154b86dca59bf2fce0ca42a22af2d5002684cee
Dan Pastusek <dan@hexial.com>
2023-09-15 17:20:25 -0700

    more verbose install script

--- bin/install.sh
+++ bin/install.sh
@@ -2,7 +2,10 @@
 set -e # bail on error
 
 PIBOX_HOST_VERSION=v$(cat package.json | jq -r .version)
-tar -xzf pibox-host-$PIBOX_HOST_VERSION.tar.gz --directory=/opt/pibox-host
+TARGET_DIR=/opt/pibox-host/$PIBOX_HOST_VERSION
+mkdir -p $TARGET_DIR
+echo "Extracting tarball to $TARGET_DIR ... (this may take a while)"
+tar -xzf pibox-host-$PIBOX_HOST_VERSION.tar.gz --directory=$TARGET_DIR
 cp pibox-host.service /etc/systemd/system/pibox-host.service
 sed -i "s/PIBOX_HOST_VERSION/$PIBOX_HOST_VERSION/g" /etc/systemd/system/pibox-host.service
 systemctl daemon-reload
