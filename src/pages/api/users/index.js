dbfc42192cf7d588b169287f353507080ce2c5d6
Dan Pastusek <dan@hexial.com>
2023-10-11 01:41:57 +0100

    allow creating users without password

--- src/pages/api/users/index.js
+++ src/pages/api/users/index.js
@@ -1,45 +1,37 @@
-import { readFile } from "fs/promises";
-import {
-  createUser,
-  setSystemPassword,
-  middlewareAuth,
-  getConfig,
-} from "@/functions";
-import randomColor from "randomcolor";
+import { readFile } from 'fs/promises';
+import { createUser, setSystemPassword, middlewareAuth, getConfig, saveConfig } from '@/functions';
+import randomColor from 'randomcolor';
 
-const PRESET_COLORS = "#1BBE4D,#D96CFF,#FF7896,#F9F871,#5C83FF".split(",");
+const PRESET_COLORS = '#1BBE4D,#D96CFF,#FF7896,#F9F871,#5C83FF'.split(',');
 
 export default async function handler(req, res) {
-  if (req.method === "GET") {
+  if (req.method === 'GET') {
     return listUsers(req, res);
-  } else if (req.method === "POST") {
+  } else if (req.method === 'POST') {
     if (!(await middlewareAuth(req, res))) {
-      return res.status(401).json({ error: "Unauthorized" });
+      return res.status(401).json({ error: 'Unauthorized' });
     }
     if (!req.isOwner) {
-      return res.status(400).json({ error: "Only the owner can create users" });
+      return res.status(400).json({ error: 'Only the owner can create users' });
     }
 
     let { username, fullName, password } = req.body;
 
     if (!fullName) {
-      return res.status(400).json({ error: "Missing full name" });
+      return res.status(400).json({ error: 'Missing full name' });
     }
 
     if (!username) {
-      const firstName = fullName.split(" ")[0];
-      username = username || firstName.toLowerCase().replace(/[^a-z0-9]/g, "");
+      const firstName = fullName.split(' ')[0];
+      username = username || firstName.toLowerCase().replace(/[^a-z0-9]/g, '');
     }
 
     try {
       await createUser(username, fullName);
     } catch (err) {
       // console.log(err);
-      if (err.message.includes("already exists")) {
-        await createUser(
-          username + Math.floor(Math.random() * 10000),
-          fullName
-        );
+      if (err.message.includes('already exists')) {
+        await createUser(username + Math.floor(Math.random() * 10000), fullName);
       } else {
         console.error(`Error creating user: ${err}`);
       }
@@ -54,42 +46,37 @@ export default async function handler(req, res) {
       }
     }
     const config = await getConfig();
-    config.users.push({ username, fullName });
-    await writeFile(CONFIG_FILE_PATH, JSON.stringify(config, null, 2));
-    return res.status(201).json({ message: "User created" });
+    // config.users.push({ username, fullName });
+    saveConfig(config);
+    return res.status(201).json({ message: 'User created' });
   } else {
-    return res.status(405).json({ error: "Method not allowed" });
+    return res.status(405).json({ error: 'Method not allowed' });
   }
 }
 
 async function listUsers(req, res) {
   const config = await getConfig();
   if (!config) {
-    return res.status(400).json({ error: "Device not set up yet" });
+    return res.status(400).json({ error: 'Device not set up yet' });
   }
 
   let users;
   try {
-    users = await readFile("/etc/passwd", "utf8");
+    users = await readFile('/etc/passwd', 'utf8');
   } catch (err) {
     console.error(`Error getting users: ${err}`);
-    return res.status(500).json({ error: "Error getting users" });
+    return res.status(500).json({ error: 'Error getting users' });
   }
 
   // parse passwd file
 
   users = users
-    .split("\n")
+    .split('\n')
     .map((user) => {
-      const [username, _password, _uid, _gid, fullName, _home, shell] =
-        user.split(":");
+      const [username, _password, _uid, _gid, fullName, _home, shell] = user.split(':');
       return { username, fullName, shell };
     })
-    .filter(
-      (user) =>
-        ["/bin/bash", "/bin/zsh"].includes(user.shell) &&
-        user.username !== "root"
-    )
+    .filter((user) => ['/bin/bash', '/bin/zsh'].includes(user.shell) && user.username !== 'root')
     .sort((user) => {
       // sort owner first
       if (user.username === config.owner) return -1;
@@ -104,8 +91,8 @@ async function listUsers(req, res) {
         color:
           PRESET_COLORS[index]?.toLowerCase() ||
           randomColor({
-            luminosity: "light",
-            format: "hex",
+            luminosity: 'light',
+            format: 'hex',
             seed: index * 3,
           }),
       };
