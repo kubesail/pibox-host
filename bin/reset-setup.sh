b5772af1f7ba0da9e0759b2afa636af70be27d8a
Dan Pastusek <dan@hexial.com>
2024-01-24 20:17:40 -0800

    remove all pibox setup files

--- bin/reset-setup.sh
+++ bin/reset-setup.sh
@@ -16,7 +16,7 @@ echo "YES" | cryptsetup luksClose /dev/mapper/encrypted_sda
 echo "YES" | cryptsetup luksClose /dev/mapper/encrypted_sdb
 dd if=/dev/zero of=/dev/sda bs=1M count=100
 dd if=/dev/zero of=/dev/sdb bs=1M count=100
-rm /etc/pibox-host/initial-setup-complete
+rm -vf /etc/pibox-host/*
 deluser --remove-home ${USER}
 
 curl localhost/api/util/disk-locking-status | jq
