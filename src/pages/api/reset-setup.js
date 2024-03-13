aa5ea795b8005d12e0540a9f0720bdfba80593fc
Dan Pastusek <Dan Pastusek>
2024-03-12 23:48:02 -0700

    clear initial config files

--- src/pages/api/reset-setup.js
+++ src/pages/api/reset-setup.js
@@ -35,18 +35,7 @@ export default async function handler(req, res) {
 
   const errors = []
 
-  try {
-    await resetDrives()
-  } catch (err) {
-    console.error(`Error resetting drive: ${err}`)
-    throw err
-  }
-
-  try {
-    await unlink(CONFIG_FILE_PATH)
-  } catch (err) {
-    errors.push('Error deleting setup config file')
-  }
+  await resetDrives()
 
   try {
     // remove linux user and home directory (--remove-home)
@@ -55,7 +44,9 @@ export default async function handler(req, res) {
   } catch (err) {
     errors.push(`Error deleting user: ${err}`)
   }
-
+  try {
+    await unlink('/etc/pibox-host/owner')
+  } catch {}
   try {
     await unlink('/etc/pibox-host/initial-setup-complete')
   } catch {}
